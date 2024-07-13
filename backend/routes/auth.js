d5ac22318f488fd7c720aeb5c909eb128d105c61
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-07-13 10:12:05 +0500

    Adding Data Validation Using express-validator on UserData

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,29 @@
 const express = require('express');
-const User = require("../models/User")
+const User = require("../models/User");
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-// Creating a User using: POST "/api/auth/". Does not req Auth
-router.post('/', (req, res) => {
-    res.send(req.body)
-    const user = User(req.body)
-    user.save();
-    console.log(req.body)
-})
+// Creating a User using: POST "/api/auth/". Does not require Auth
+router.post('/', [
+    body('name', 'Enter a valid Name').isLength({ min: 5 }),
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Enter a valid Password').isLength({ min: 5 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    })
+        .then(user => res.json(user))
+        .catch(err => {
+            console.error(err.message);
+            res.status(409).send("Enter Unique value for Email");
+        });
+});
 
 module.exports = router;
