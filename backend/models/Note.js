71c6b174da2704c0033d47af5a401fda9feb9eb0
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-07-14 12:45:23 +0500

    Fetching all notes & Adding a Note

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,8 +1,13 @@
-import { type } from '@testing-library/user-event/dist/type';
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+
+    user: {
+        // The user field establishes a relationship between the notes and the user who created them. This way, each note can be linked back to a specific user.
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title: {
         type: String,
         required: true
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,46 @@
 const express = require('express');
+const fetchuser = require('../middleware/fetchuser');
+const Note = require("../models/Note");
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hi')
+// ROUTE 1: Get all notes of a User using: GET "/api/auth/fetchtallnotes". Login require
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.send(notes)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Some Error occured')
+    }
 })
 
+// ROUTE 2: Sdd new note using: POST "/api/auth/addnote". Login require
+router.post('/addnote', fetchuser, [
+    // validations
+    body('title', 'Enter a valid Title').isLength({ min: 3 }),
+    body('description', 'Enter a valid Description').isLength({ min: 5 }),
+], async (req, res) => {
+
+    try {
+        const { title, description, tag } = req.body;
+
+        // If there are errors, return bad requests and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+
+        const savedNote = await note.save();
+        res.json(savedNote)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Some Error occured')
+    }
+})
 module.exports = router;
