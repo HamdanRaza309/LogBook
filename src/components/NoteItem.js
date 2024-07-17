69e0e3ca9fb918c82913271b5ea1e54e61e0c84b
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-07-17 08:31:55 +0500

    Adding font awesome icons

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -8,6 +8,9 @@ export default function Home(props) {
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
+                    {/* <FontAwesomeIcon icon="fa-solid fa-trash" />*/}
+                    <i className="fa-solid fa-trash mx-2"></i>
+                    <i className="fa-solid fa-pen-to-square mx-2"></i>
                 </div>
             </div>
         </div>
