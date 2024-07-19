621582b39300458ad76e9c0fb12dd10a8ba5688f
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-07-19 10:23:12 +0500

    Adding Frontend Validation + Few Fixes

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -5,10 +5,11 @@ export default function AddNote() {
     const context = useContext(noteContext);
     const { addNote } = context;
 
-    const [note, setNote] = useState({ title: "", description: "", tage: 'default' })
+    const [note, setNote] = useState({ title: "", description: "", tag: "" })
     const handleOnClick = (e) => {
         e.preventDefault()
         addNote(note.title, note.description, note.tag)
+        setNote({ title: "", description: "", tag: "" })
     }
 
     const handleOnChange = (e) => {
@@ -21,17 +22,19 @@ export default function AddNote() {
             <form>
                 <div className="form-group">
                     <label htmlFor="title">Title</label>
-                    <input type="text" className="form-control" id="title" name='title' aria-describedby="emailHelp" onChange={handleOnChange} />
+                    <input type="text" className="form-control" id="title" name='title' value={note.title} aria-describedby="emailHelp" onChange={handleOnChange} minLength={5} required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="description">Description</label>
-                    <input type="text" className="form-control" id="description" name='description' onChange={handleOnChange} />
+                    <input type="text" className="form-control" id="description" name='description' value={note.description} onChange={handleOnChange} minLength={5} required />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="tag" className="form-label">Tag</label>
-                    <input type="text" className="form-control" id="tag" name="tag" onChange={handleOnChange} />
+                    <input type="text" className="form-control" id="tag" name="tag" value={note.tag} onChange={handleOnChange} />
                 </div>
-                <button type="submit" className="btn btn-primary" onClick={handleOnClick}>Add Note</button>
+                <small>Length of title and description must be greater 5</small>
+                <br />
+                <button style={{ margin: '10px 0px' }} disabled={note.title.length < 5 || note.description.length < 5} type="submit" className="btn btn-primary" onClick={handleOnClick}>Add Note</button>
             </form>
         </div>
     )
