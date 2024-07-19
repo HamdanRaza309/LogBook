21ff4b77d136f53799cac21c249459449caf9a07
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-07-19 09:20:05 +0500

    Updating Notes on edit in the UI using React

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,34 +1,33 @@
 import React, { useContext, useEffect, useRef, useState } from 'react'
-import noteContext from '../context/notes/noteContext';
-import NoteItem from "./NoteItem";
+import noteContext from "../context/notes/noteContext"
+import NoteItem from './NoteItem';
 import AddNote from './AddNote';
 
-export default function Home() {
+const Notes = () => {
     const context = useContext(noteContext);
-    const { notes, getNotes } = context;
-
+    const { notes, getNotes, editNote } = context;
     useEffect(() => {
-        getNotes();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
+        getNotes()
+        // eslint-disable-next-line
+    }, [])
     const ref = useRef(null)
-    const [note, setNote] = useState({ etitle: "", edescription: "", etag: "" })
+    const refClose = useRef(null)
+    const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "" })
 
     const updateNote = (currentNote) => {
         ref.current.click();
         setNote({ etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag })
+        setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag })
     }
 
     const handleClick = (e) => {
-        console.log("Updating the note...", note)
-        e.preventDefault();
+        editNote(note.id, note.etitle, note.edescription, note.etag)
+        refClose.current.click();
     }
 
     const onChange = (e) => {
         setNote({ ...note, [e.target.name]: e.target.value })
     }
-
     return (
         <>
             <AddNote />
@@ -60,13 +59,12 @@ export default function Home() {
                             </form>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button onClick={handleClick} type="button" className="btn btn-primary">Update </button>
+                            <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
                         </div>
                     </div>
                 </div>
             </div>
-
             <div className="row my-3">
                 <h2>You Notes</h2>
                 {notes.map((note) => {
@@ -74,5 +72,6 @@ export default function Home() {
                 })}
             </div>
         </>
-    );
+    )
 }
+export default Notes
